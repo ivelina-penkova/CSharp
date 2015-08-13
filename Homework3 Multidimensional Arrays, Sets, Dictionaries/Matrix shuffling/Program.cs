5de31bd8e14b1c5fe214ef42183ebf28f6a44ba9
Ivelina Ivanova <ivcheto.ivanova@gmail.com>
2015-08-13 22:07:16 +0300

    added matrix shuffling

---
+++ Homework3 Multidimensional Arrays, Sets, Dictionaries/Matrix shuffling/Program.cs
@@ -0,0 +1,81 @@
+﻿/*Problem 3.	Matrix shuffling
+Write a program which reads a string matrix from the console and performs certain operations with its elements.
+User input is provided like in the problem above – first you read the dimensions and then the data. 
+Remember, you are not required to do this step first, you may add this functionality later.  
+Your program should then receive commands in format: "swap x1 y1 x2 y2" where x1, x2, y1, y2 are coordinates in the matrix. In order for a command to be valid, it should start with the "swap" keyword along with four valid coordinates (no more, no less). You should swap the values at the given coordinates (cell [x1, y1] with cell [x2, y2]) and print the matrix at each step (thus you'll be able to check if the operation was performed correctly). 
+If the command is not valid (doesn't contain the keyword "swap", has fewer or more coordinates entered or 
+the given coordinates do not exist), print "Invalid input!" and move on to the next command. 
+Your program should finish when the string "END" is entered. 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3_Multidimensional_Arrays
+{
+    class Matrix_shuffling
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of the rows of the matrix:");
+            int rows = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the size of the columns of the matrix:");
+            int cols = int.Parse(Console.ReadLine());
+            string[,] matrix = new string[rows, cols];
+            FillMatrix(matrix,rows,cols);
+            Shuffle(matrix);
+
+        }
+
+        static void FillMatrix(string[,] m, int r, int c)
+        {
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < c; j++)
+                {
+                    m[i, j] = Console.ReadLine();
+                }
+            }
+        }
+
+        static void Shuffle(string[,] m)
+        {
+            string input = Console.ReadLine();
+            while (input != "END")
+            {
+                string[] inputArr = input.Split(' ');
+
+                if (inputArr.Contains("swap") && inputArr.Length == 5 && (int.Parse(inputArr[1]) < m.GetLength(0)) && 
+                    (int.Parse(inputArr[2]) < m.GetLength(1)) && (int.Parse(inputArr[3]) < m.GetLength(0)) && 
+                    (int.Parse(inputArr[4]) < m.GetLength(1)))
+                {
+                    string temp = m[int.Parse(inputArr[1]), int.Parse(inputArr[2])];
+                    m[int.Parse(inputArr[1]), int.Parse(inputArr[2])] = m[int.Parse(inputArr[3]), int.Parse(inputArr[4])];
+                    m[int.Parse(inputArr[3]), int.Parse(inputArr[4])] = temp;
+                    PrintMatrix(m);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+
+        static void PrintMatrix(string[,] m)
+        {
+            for (int row = 0;  row < m.GetLength(0);  row++)
+            {
+                for (int col = 0; col < m.GetLength(1); col++)
+                {
+                    Console.Write(m[row,col]+" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Homework3 Multidimensional Arrays, Sets, Dictionaries/Maximal Sum/Program.cs
@@ -0,0 +1,72 @@
+﻿/*
+Problem 2.	Maximal Sum
+Write a program that reads a rectangular integer matrix of size N x M and finds in it the square 3 x 3 that
+has maximal sum of its elements. 
+On the first line, you will receive the rows N and columns M. On the next N lines you will receive each
+row with its columns.
+Print the elements of the 3 x 3 square as a matrix, along with their sum.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3_Multidimensional_Arrays
+{
+    class Fill_the_Matrix_v2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the count of rows:");
+            int rows = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the count of columns:");
+            int cols = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[rows, cols];
+            Console.WriteLine("Enter elements:");
+            FillMatrix(matrix);
+
+            int maxSum = int.MinValue;
+            int maxRow=0, maxCol=0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 2; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 2; col++)
+                {
+                    int sum = matrix[row, col] + matrix[row, col + 1] + matrix[row, col + 2]
+                             + matrix[row + 1, col] + matrix[row + 1, col + 1] + matrix[row + 1, col + 2]
+                             + matrix[row + 2, col] + matrix[row + 2, col + 1] + matrix[row + 2, col + 2];
+                    if (sum > maxSum)
+                    {
+                        maxSum = sum;
+                        maxRow = row;
+                        maxCol = col;
+                    }
+                        
+                }
+            }
+
+            Console.WriteLine("The max sum is: {0}",maxSum);
+            for (int row = maxRow; row <maxRow + 3; row++)
+            {
+                for (int col = maxCol; col <maxCol + 3; col++)
+                {
+                    Console.Write("{0,4}", matrix[row,col]);
+                }
+                Console.WriteLine(); 
+            }
+        }
+
+        static void FillMatrix(int[,] m)
+        {
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+                for (int j = 0; j < m.GetLength(1); j++)
+                {
+                    m[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+    }
+}
