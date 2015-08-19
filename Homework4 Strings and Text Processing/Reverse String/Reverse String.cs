efe331208311bad79ae24c236bbf05edf50312c6
Ivelina Ivanova <ivcheto.ivanova@gmail.com>
2015-08-19 19:07:46 +0300

    added reverse string &  string length

---
+++ Homework4 Strings and Text Processing/Reverse String/Reverse String.cs
@@ -0,0 +1,24 @@
+﻿/*Write a program that reads a string from the console, reverses it and prints 
+the result back at the console.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your text here:");
+            string input = Console.ReadLine();
+            char[] charArr = input.ToCharArray();
+            Array.Reverse(charArr);
+            string output = new string(charArr);
+            Console.WriteLine("The reversed text is: {0}",output);
+        }
+    }
+}
---
+++ Homework4 Strings and Text Processing/String Length/String-Length.cs
@@ -0,0 +1,29 @@
+﻿/*Write a program that reads from the console a string of maximum 20 characters. 
+If the length of the string is less than 20, the rest of the characters should be filled with *.
+Print the resulting string on the console.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Length
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your text:");
+            string input = Console.ReadLine();
+            if (input.Length >= 20)
+            {
+                Console.WriteLine(input.Substring(0,20));
+            }
+            else
+            {
+                Console.WriteLine(input.PadRight(20,'*'));
+            }
+        }
+    }
+}
