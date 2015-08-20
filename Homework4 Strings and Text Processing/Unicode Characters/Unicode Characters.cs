bc7ab1ad94a3349253fa5ead33adfe8b04f646b3
Ivelina Ivanova <ivcheto.ivanova@gmail.com>
2015-08-20 19:54:47 +0300

    added unicode characters

---
+++ Homework4 Strings and Text Processing/Unicode Characters/Unicode Characters.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Unicode_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your text:");
+            string input = Console.ReadLine();
+            for (int i = 0; i < input.Length; i++)
+            {
+                Console.Write(@"\{0:X4} ", (int)input[i]);
+            }
+        }
+    }
+}
