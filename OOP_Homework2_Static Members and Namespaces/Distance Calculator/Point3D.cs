b812c3a9757850312ebd99c5bd56d3ab45a9750e
Ivelina Ivanova <ivcheto.ivanova@gmail.com>
2015-09-10 18:11:38 +0300

    finished problem2 oop hw2

---
+++ OOP_Homework2_Static Members and Namespaces/Distance Calculator/Point3D.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Distance_Calculator
+{
+    class Point3D
+    {
+        private double x;
+        private double y;
+        private double z;
+
+        public double X
+        {
+            get
+            {
+                return this.x;
+            }
+            set
+            {
+                this.x = value;
+            }
+        }
+
+        public double Y
+        {
+            get
+            {
+                return this.y;
+            }
+            set
+            {
+                this.y = value;
+            }
+        }
+
+        public double Z
+        {
+            get
+            {
+                return this.z;
+            }
+            set
+            {
+                this.z = value;
+            }
+        }
+
+        public Point3D(double x, double y, double z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+
+    }
+
+}
---
+++ OOP_Homework2_Static Members and Namespaces/Distance Calculator/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+Write a static class DistanceCalculator with a static method to calculate the distance between two points in the 3D space. 
+Search in Internet how to calculate distance in the 3D Euclidian space.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Distance_Calculator.Point3D;
+
+namespace Distance_Calculator
+{
+    static class DistanceCalculator
+    {
+               
+        static DistanceCalculator()
+        {
+        }
+
+
+        public static double CalcDistance(Point3D A, Point3D B)
+        {
+            double res = Math.Sqrt(Math.Pow((A.X - B.X), 2) + Math.Pow((A.Y - B.Y), 2) + Math.Pow((A.Z - B.Z), 2));
+            return res;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Point3D A = new Point3D(2, 6, 8.6);
+            Point3D B = new Point3D(9, 7, 4.5);
+            Console.WriteLine(DistanceCalculator.CalcDistance(A,B));
+        }
+    }
+}
